503a7a399bd2396d7c7f98ab92c1101fbda6ba8c
Rahul Sharma <rahulsrma26@gmail.com>
2020-12-18 00:20:00 +0530

    :construction_worker: setup file added

---
+++ setup.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+import sys
+import platform
+from setuptools import setup, find_packages
+
+_this = os.path.abspath(os.path.dirname(__file__))
+
+def get_requirements(filename):
+    contents = open(os.path.join(_this, filename)).read()
+    return [
+        req for req in contents.split('\n')
+        if req != '' and not req.startswith('#')
+    ]
+
+setup(
+    version='0.0.1',
+    install_requires=get_requirements('requirements-client.txt')
+)
