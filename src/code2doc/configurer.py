afefcd84d3a12b16e85576d68a4cc063a0cfb8a3
Rahul Sharma <rahulsrma26@gmail.com>
2020-12-20 01:18:41 +0530

    :heavy_plus_sign: indexing, saving and loading from ini features added

--- src/code2doc/configurer.py
+++ src/code2doc/configurer.py
@@ -1,4 +1,8 @@
+'''
+'''
+
 import argparse
+import configparser
 from typing import List
 
 
@@ -8,71 +12,74 @@ class ConfigOption:
     def get_short_n_full_form(option: str) -> str:
         full = '_'.join(option.strip().split())
         short = ''.join([w[0] for w in full.split('_')])
-        return '-' + short, '--' + full
+        return short, full
 
-    def __init__(self, option: str, default, doc: str = ''):
+    def __init__(self, option: str, value, doc: str = ''):
         self.short, self.full = ConfigOption.get_short_n_full_form(option)
-        self.default = default
+        self.value = value
         self.doc = doc
 
     def __str__(self):
-        return f'{self.short:4} {self.full}  default={repr(self.default)}  doc={repr(self.doc)}'
+        return f'-{self.short:4} --{self.full}  value={repr(self.value)}  doc={repr(self.doc)}'
 
     def add_argument(self, parser):
-        if isinstance(self.default, bool): 
-            if self.default is True:
-                parser.add_argument(self.short, self.full, action='store_false', help=self.doc)
+        short, full = f'-{self.short}', f'--{self.full}'
+        if isinstance(self.value, bool): 
+            if self.value is True:
+                parser.add_argument(short, full, action='store_false', help=self.doc)
             else:
-                parser.add_argument(self.short, self.full, action='store_true', default=False, help=self.doc)
-        elif isinstance(self.default, list): 
-            parser.add_argument(self.short, self.full, action='append', help=self.doc)
+                parser.add_argument(short, full, action='store_true', default=False, help=self.doc)
+        elif isinstance(self.value, list): 
+            parser.add_argument(short, full, action='append', help=self.doc)
         else:
-            parser.add_argument(self.short, self.full, default=self.default, help=self.doc)
+            parser.add_argument(short, full, default=self.value, help=self.doc)
 
 
 class Configuration:
 
-    def __init__(self):
-        self.options = {}
+    def __init__(self, name='code2doc'):
+        self.name = name
+        self.shorts = {}
+        self.options = []
 
     def add(self, option: ConfigOption):
-        if option.short in self.options:
+        if option.short in self.shorts:
             print(f'WARNING! option {option.full} already exists in config ({option.short}). Overriding!')
-        self.options[option.short] = option
+        self.shorts[option.short] = option
+        self.options.append(option)
         return self
 
     def __str__(self):
-        return '\n'.join([str(o) for o in self.options.values()])
+        return '\n'.join([str(o) for o in self.options])
+
+    def __getitem__(self, index):
+        short, _ = ConfigOption.get_short_n_full_form(index)
+        return self.shorts[short]
 
     def add_arguments(self, parser):
-        for option in self.options.values():
+        for option in self.options:
             option.add_argument(parser)
+            
+    def save(self, fname):
+        config = configparser.ConfigParser(allow_no_value=True)
+        config.add_section(self.name)
+        for option in self.options:
+            config.set(self.name, '; ' + option.doc)
+            if isinstance(option.value, str): 
+                config.set(self.name, option.full, repr(option.value))
+            else:
+                config.set(self.name, option.full, str(option.value))
+        with open(fname, 'w') as f:
+            config.write(f)
 
-
-DEFAULT_CONFIG = Configuration().add(
-    ConfigOption('ignore_non_documented', False, 'Ignore files that contains no top level documentation')).add(
-    ConfigOption('generate_root_directories', True, 'Generate directory for each file/module in the output directory')).add(
-    ConfigOption('show_relative_imports', True, 'Show all the relative imports in a file')).add(
-    ConfigOption('link_relative_imports', False, 'Link all the relative imports in a file')).add(
-    ConfigOption('show_module_variables', True, 'Show all the variables in a module')).add(
-    ConfigOption('show_module_functions', True, 'Show all the functions in a module')).add(
-    ConfigOption('show_module_classes', True, 'Show all the classes in a module')).add(
-    ConfigOption('show_class_variables', True, 'Show all the variables in a class')).add(
-    ConfigOption('show_class_functions', True, 'Show all the object functions in a class')).add(
-    ConfigOption('show_classmethods', True, 'Show all the classmethods of a class')).add(
-    ConfigOption('show_staticmethods', True, 'Show all the staticmethods of a class')).add(
-    ConfigOption('show_types', True, 'Show types in the function signature')).add(
-    ConfigOption('link_types', False, 'Link types in the function signature')).add(
-    ConfigOption('ignore_dot', True, 'Ignore all the files starting with a dot (hidden files)')).add(
-    ConfigOption('ignore_underscore', True, 'Ignore all the files starting with an underscore')).add(
-    ConfigOption('ignore_files', [], 'Ignore the specified file(s)')).add(
-    ConfigOption('header_file', '', 'Append all the markdown with this in the beginning')).add(
-    ConfigOption('footer_file', '', 'Append all the markdown with this in the end')).add(
-    ConfigOption('build_version', True, 'Write build utility version at the end of markdown'))
-
-
-if __name__ == "__main__":
-    # print(DEFAULT_CONFIG)
-    parser = argparse.ArgumentParser(prog='Test', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    DEFAULT_CONFIG.add_arguments(parser)
-    parser.print_help()
+    def load(self, fname):
+        config = configparser.ConfigParser()
+        config.read(fname)
+        if self.name in config:
+            for option in self.options:
+                if option.full in config[self.name]:
+                    option.value = config[self.name][option.full]
+                    if option.value:
+                        option.value = eval(option.value)
+        else:
+            print(f'WARNING! section `{self.name}` does not found in `{fname}`')
