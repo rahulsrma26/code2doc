7c4e11449595253c4bf694f1f86b5168d67dd494
Rahul Sharma <rahulsrma26@gmail.com>
2020-12-28 00:33:33 +0530

    :heavy_plus_sign: target output path added

--- src/code2doc/builder.py
+++ src/code2doc/builder.py
@@ -12,12 +12,18 @@ from .doc_types import DocModule
 
 
 class DocNode:
-    def __init__(self, path:str, name: list, is_file: bool, package: str):
+    def __init__(self, path: str, name: list, is_file: bool, package: str, config: dict):
         self.name = name
         self.package = package
         import_string = '.' + '.'.join(name)
         self.module = DocModule.from_path(path, package, import_string)
         self.is_file = is_file
+        self.target = package if config['generate_root_directories'] else ''
+        if ''.join(name):
+            self.target = os.path.join(self.target, os.path.sep.join(name))
+        if not is_file:
+            self.target = os.path.join(self.target, 'ReadMe')
+        self.target += '.md'
         self.children = []
 
     def add(self, node):
@@ -26,7 +32,7 @@ class DocNode:
 
     def __str__(self):
         tag = 'f' if self.is_file else 'D'
-        parts = [f'{tag} {self.name}']
+        parts = [f'{tag} {self.name} {self.target}']
         for child in self.children:
             parts.append(str(child))
         return '\n'.join(parts)
@@ -57,11 +63,15 @@ class DocBuilder:
             if ext == '.py':
                 name = base[len(self.abspath) + 1:]
                 if self.filter(name):
-                    return DocNode(self.basedir, name.split(os.path.sep), is_file=True, package=self.package)
+                    return DocNode(
+                        path=self.basedir, name=name.split(os.path.sep),
+                        is_file=True, package=self.package, config=self.config)
         elif os.path.isdir(path):
             if glob(os.path.join(path, "**", "*.py"), recursive=True):
                 name = path[len(self.abspath) + 1:].split(os.path.sep)
-                root = DocNode(self.basedir, name, is_file=False, package=self.package)
+                root = DocNode(
+                    path=self.basedir, name=name,
+                    is_file=False, package=self.package, config=self.config)
                 for fname in os.listdir(path):
                     filepath = os.path.join(path, fname)
                     root.add(self.build_tree(filepath))
@@ -71,9 +81,10 @@ class DocBuilder:
 if __name__ == "__main__":
     config = {
         "ignore_dot": True,
-        "ignore_underscore": True
+        "ignore_underscore": True,
+        "generate_root_directories": False
     }
 
-    builder = DocBuilder('./src/code2doc', config)
+    builder = DocBuilder('./src/code2doc/', config)
     print(builder.package)
     print(builder.tree)
