9a6fad6431d6a41cf80be8c7791915d0ccdd9a3a
Rahul Sharma <rahulsrma26@gmail.com>
2020-12-30 00:31:39 +0530

    :pushpin: Using const string for config options

--- src/code2doc/builder.py
+++ src/code2doc/builder.py
@@ -9,6 +9,7 @@ import sys
 from glob import glob
 import json
 from .doc_types import DocModule
+from .build_config import Options
 
 
 class DocNode:
@@ -18,7 +19,7 @@ class DocNode:
         import_string = '.' + '.'.join(name)
         self.module = DocModule.from_path(path, package, import_string)
         self.is_file = is_file
-        self.target = package if config['generate_root_directories'] else ''
+        self.target = package if config[Options.GENERATE_ROOT_DIRECTORIES] else ''
         if ''.join(name):
             self.target = os.path.join(self.target, os.path.sep.join(name))
         if not is_file:
@@ -51,9 +52,9 @@ class DocBuilder:
         self.tree = self.build_tree(self.abspath)
 
     def filter(self, name):
-        if self.config['ignore_dot'] and name.startswith('.'):
+        if self.config[Options.IGNORE_DOT] and name.startswith('.'):
             return False
-        if self.config['ignore_underscore'] and name.startswith('_'):
+        if self.config[Options.IGNORE_UNDERSCORE] and name.startswith('_'):
             return False
         return True
 
@@ -80,9 +81,9 @@ class DocBuilder:
 
 if __name__ == "__main__":
     config = {
-        "ignore_dot": True,
-        "ignore_underscore": True,
-        "generate_root_directories": False
+        Options.IGNORE_DOT: True,
+        Options.IGNORE_UNDERSCORE: True,
+        Options.GENERATE_ROOT_DIRECTORIES: False
     }
 
     builder = DocBuilder('./src/code2doc/', config)
