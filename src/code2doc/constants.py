64d5ad1e5e840bb83ee5c4128e17f76ac5ef4888
Rahul Sharma <rahulsrma26@gmail.com>
2021-01-02 22:02:12 +0530

    :ledger: Config constants added

--- src/code2doc/constants.py
+++ src/code2doc/constants.py
@@ -5,3 +5,5 @@ THIS_DIR = os.path.dirname(__file__)
 VERSION = open(os.path.join(THIS_DIR, '__version__.py')).readline()
 README = 'ReadMe'
 OUTPUT_EXT = '.md'
+CONFIG_EXT = '.ini'
+DEFAULT_CONFIG_FILENAME = PROGRAM_NAME + CONFIG_EXT
