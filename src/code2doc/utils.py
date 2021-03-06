3b3210381541742bcab8df69438ba4f89332c327
Rahul Sharma <rahulsrma26@gmail.com>
2021-03-07 00:46:32 +0530

    :straight_ruler: reindentation support added

--- src/code2doc/utils.py
+++ src/code2doc/utils.py
@@ -7,8 +7,25 @@ Contains different common helper functions.
 import os
 
 def read_file(path: str) -> str:
+    '''
+    Reads a file and return it's content as a string.
+    '''
     if os.path.isfile(path):
         with open(path, 'r') as f:
             return f.read()
     else:
         return ''
+
+
+def reindent(s: str) -> str:
+    '''
+    This reindents a string by keeping only the minimum level of indentation.
+    '''
+    lines, m = s.splitlines(True), None
+    for line in lines:
+        if not line.isspace():
+            indent = len(line) - len(line.lstrip())
+            m = min(m, indent) if m else indent
+    if m:
+        lines = [l if l.isspace() else l[indent:] for l in lines]
+        return ''.join(lines)
