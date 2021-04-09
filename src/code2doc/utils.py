7a57d8faafca7104b8027f209897e733d7f163cc
Rahul Sharma <rahulsrma26@gmail.com>
2021-04-09 11:49:59 +0530

    :heavy_plus_sign: reindent can now trim empty lines

--- src/code2doc/utils.py
+++ src/code2doc/utils.py
@@ -17,7 +17,7 @@ def read_file(path: str) -> str:
         return ''
 
 
-def reindent(s: str, min_spaces: int = 0) -> str:
+def reindent(s: str, min_spaces: int = 0, trim_lines: bool = True) -> str:
     '''
     This reindents text by keeping only the minimum level of indentation.
 
@@ -27,11 +27,19 @@ def reindent(s: str, min_spaces: int = 0) -> str:
         minimum number of spaces to be added as a prefix after reindentation.
     '''
     lines, m = s.splitlines(True), -1
-    for line in lines:
+    start = end = -1
+    for idx, line in enumerate(lines):
         if not line.isspace():
             indent = len(line) - len(line.lstrip())
             m = min(m, indent) if m >= 0 else indent
+            end = idx
+        elif start == idx - 1:
+            start = idx
     if m:
         lines = [l if l.isspace() else l[m:] for l in lines]
     prefix = ' ' * min_spaces
+    if trim_lines:
+        lines = lines[start + 1: end + 1]
+        if lines and lines[-1]:
+            lines[-1] = lines[-1].rstrip()
     return ''.join([prefix + l for l in lines])
